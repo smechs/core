e6a5756ad8e4437b910f1cd191f9c9ef90bed01d
smechs <sebastian.mechs@gmail.com>
2024-01-17 20:53:44 +0000

    updating sensor description and value

--- config/custom_components/oilchecking/sensor.py
+++ config/custom_components/oilchecking/sensor.py
@@ -2,9 +2,15 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
 import logging
 
-from homeassistant.components.light import LightEntity
+from homeassistant.components.number import (
+    NumberDeviceClass,
+    NumberEntity,
+    NumberEntityDescription,
+    NumberMode,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -35,13 +41,40 @@ async def async_setup_entry(
     #
     await coordinator.async_config_entry_first_refresh()
 
+    entities: list[MyEntity] = []
+
+    entities.extend(
+            [
+                MyEntity(coordinator, 2, description)
+                for description in NUMBER_TYPES
+            ]
+        )
+
     async_add_entities(
-        MyEntity(coordinator, idx) for idx, ent in enumerate(coordinator.data)
+        entities
     )
 
     return True
 
-class MyEntity(CoordinatorEntity, LightEntity):
+@dataclass(frozen=True)
+class OilInfoNumberEntityDescription(NumberEntityDescription):
+    """Describes BMW number entity."""
+
+
+NUMBER_TYPES: list[OilInfoNumberEntityDescription] = [
+    OilInfoNumberEntityDescription(
+        key="oil_info",
+        translation_key="oil_info",
+        device_class=NumberDeviceClass.VOLUME,
+        native_max_value=5000.0,
+        native_min_value=1000.0,
+        native_step=50.0,
+        mode=NumberMode.SLIDER,
+        icon="mdi:battery-charging-medium",
+    ),
+]
+
+class MyEntity(CoordinatorEntity, NumberEntity):
     """An entity using CoordinatorEntity.
 
     The CoordinatorEntity class provides:
@@ -52,25 +85,25 @@ class MyEntity(CoordinatorEntity, LightEntity):
 
     """
 
-    def __init__(self, coordinator, idx) -> None:
+    def __init__(self, coordinator, idx, description) -> None:
         """Pass coordinator to CoordinatorEntity."""
         super().__init__(coordinator, context=idx)
         self.idx = idx
+        self.name = "OilInfo"
         self.coordinator = coordinator
+        self.device_class = NumberDeviceClass.VOLUME_STORAGE
+        self.entity_description = description
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
         data = self.coordinator.data
         _LOGGER.info("Handling update: %s", len(data.get("result").oil_price_dtos))
-        self._attr_is_on = True
+        self._attr_native_value = data.get("result").oil_price_dtos[0].price
         self.async_write_ha_state()
 
-    async def async_turn_on(self, **kwargs):
-        """Turn the light on.
-
-        Example method how to request data updates.
-        """
+    async def async_set_native_value(self, value: float) -> None:
+        """Update the current value."""
         # Do the turning on.
         # ...
 
